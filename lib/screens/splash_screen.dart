b98309849add5120836d2a88ecb5b43437d78e07
Kartik <kartik.goyal@dhwaniris.com>
2023-02-26 21:44:29 +0530

    animation controller

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -8,7 +8,34 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animController;
+  late Animation<double> _animDouble;
+  late Animation<Offset> _animOffset;
+
+  @override
+  void initState() {
+    _animController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 1000));
+    _animDouble = CurvedAnimation(
+      parent: _animController,
+      curve: Curves.easeIn,
+      // reverseCurve: Curves.bounceIn,
+    );
+    _animOffset =
+        Tween<Offset>(begin: Offset(-3, 0), end: Offset(3, 0)).animate(
+      _animController,
+    );
+    _animController.repeat(reverse: true);
+    // _animController.addListener(() {
+    //   if (_animController.isCompleted) {
+    //     _animController.reverse();
+    //   }
+    // });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,16 +55,22 @@ class _SplashScreenState extends State<SplashScreen> {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Card(
-              elevation: 20,
-              child: Padding(
-                padding: EdgeInsets.all(8.0),
-                child: FlutterLogo(size: 100),
+          children: [
+            FadeTransition(
+              opacity: _animDouble,
+              child: SlideTransition(
+                position: _animOffset,
+                child: const Card(
+                  elevation: 20,
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: FlutterLogo(size: 100),
+                  ),
+                ),
               ),
             ),
-            SizedBox(height: 20),
-            Text(
+            const SizedBox(height: 20),
+            const Text(
               "TABAHI CHAT APP",
               style: TextStyle(
                 fontSize: 25,
